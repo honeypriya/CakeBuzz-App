9db1ca019d59ab0968818618e6bff4d8cb547bdd
Harshita-Git <harshitamohadikar4@gmail.com>
2021-02-08 19:48:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cakebuzz_cakeorderingapp/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.cakebuzz_cakeorderingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    Thread timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    synchronized (this){
+                        wait(10000);
+                    }
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
